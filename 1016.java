856151930d3b7093c3cf5a46c86188a7853c19db
tcisio10 <63074263+tcisio10@users.noreply.github.com>
2020-05-31 20:52:14 -0300

    Update 1016.java

--- 1016.java
+++ 1016.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+class MyClass {
 
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+
+        int distancia = input.nextInt();
+        int minutos = (60 * distancia) / 30;
+
+        System.out.println(minutos + " minutos");
+
+    }
+}
